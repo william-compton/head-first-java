b3ee43eec66b0b9a830eae6fdabb7d0a4431748b
William Compton <williamacompton1@gmail.com>
2022-10-17 16:24:45 -0400

    updated javabook program

--- src/JavaBook.java
+++ src/JavaBook.java
@@ -1,3 +1,5 @@
+//Head First Java -- Test scanner module and reading from files
+
 import java.util.List;
 import java.util.Scanner;
 import java.io.IOException;
@@ -5,40 +7,34 @@ import java.nio.file.*;
 
 public class JavaBook {
 	public static void main(String[] args) {
+		//Create scanner object
 		Scanner scanner = new Scanner(System.in);
+		//Prompt user for name
 		System.out.println("Enter your name: ");
+		//Create name string
 		String name = scanner.nextLine();
+		//Return greeting
 		System.out.println("Hello " + name);
+		//Close scanner object
 		scanner.close();
+		//Load file
 		try {
 			loadFile("test");
-			DooBee();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	static void loadFile(String fileName) throws IOException {
+		//Path for file
 		Path file = FileSystems.getDefault().getPath("", "test");
+		//Create list of strings from lines of file
 		List<String> lines = Files.readAllLines(file);
 		/*for (int i = 0; i < lines.size(); i++) {
 			System.out.println(lines.get(i));
 		}*/
 		//lines.forEach((str) -> System.out.println(str));
+		//Print lines from list
 		lines.forEach(System.out::println);
-	}
-	
-	
-
-	static void DooBee() {
-		int x = 1;
-		while (x < 3) {
-			System.out.print("Doo");
-			System.out.print("Bee");
-			x++;
-		}
-		if (x == 3) {
-			System.out.print("Do");
-		}
 	}
 }
