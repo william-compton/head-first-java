d29d1c8419f46ddbdc34da2ae073f15a0013e7d5
William Compton <williamacompton1@gmail.com>
2022-10-17 16:29:29 -0400

    updated example dog class code

--- src/Dog.java
+++ src/Dog.java
@@ -1,8 +1,11 @@
+//Example Class
+
 class Dog {
+	//Class variables
 	int size;
 	String breed;
 	String name;
-
+	//Class method
 	void bark() {
 		System.out.println("Ruff, Ruff!");
 	}
--- src/MixedMessages.java
+++ src/MixedMessages.java
@@ -1,3 +1,5 @@
+//Calling Dog class from Dog.java file
+
 public class MixedMessages {
 	public static void main (String [] args) {
 		int x = 0;
@@ -8,6 +10,8 @@ public class MixedMessages {
 		d.bark();
 		System.out.println(d.size);
 		System.out.println(d.breed);
+		
+		//Random while loop practice code
 		while ( x < 5 ) {
 			y = x - y;
 			System.out.print(x + "" + y + " ");
