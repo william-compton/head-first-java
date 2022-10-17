d95c64f9d8365681740078c6c925956492107d01
William Compton <williamacompton1@gmail.com>
2022-10-17 15:46:29 -0400

    added comment

--- src/Beers.java
+++ src/Beers.java
@@ -1,11 +1,10 @@
-
+//Head First Java 99 Bottles of Beer Exercise
 
 public class Beers {
 	public static void main(String[] args) {
 		int beerNum = 99;
 		String word = "bottles";
 		while (beerNum > 0) {
-			
 			System.out.println(beerNum + " " + word + " of beer on the wall");
 			System.out.println(beerNum + " " + word + " of beer.");
 			System.out.println("Take one down.");
@@ -14,12 +13,10 @@ public class Beers {
 			if (beerNum == 1) {
 				word = "bottle";
 		}
-			
 			if (beerNum > 0) {
 				System.out.println(beerNum + " " + word + " of beer on the wall");
 			} else {
 				System.out.println("No more bottles of beer on the wall");
-				
 			}
 		}
 	}
