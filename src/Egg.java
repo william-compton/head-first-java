6787b436ebfc402ab85812c9205e61d1ff278334
William Compton <williamacompton1@gmail.com>
2023-02-20 19:50:37 -0500

    array list example
    array list methods using egg class

---
+++ src/Egg.java
@@ -0,0 +1,22 @@
+
+public class Egg {
+
+	private int size = 0;
+
+	public Egg(int size) {
+		super();
+		this.size = size;
+	}
+	
+	public Egg() {
+		
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+}
